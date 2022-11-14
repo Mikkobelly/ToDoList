f515699293ff7272aaeb48869bbf884e7ebaed39
mikia <mikiakuta0529@gmail.com>
2022-11-14 14:45:55 +0000

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -127,6 +127,8 @@ app.delete('/:listName', async (req, res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log("SERVING ON PORT 3000")
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Started succecfully")
 })
